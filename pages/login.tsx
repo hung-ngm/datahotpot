c86a3aec37c3dba300a5ee00272a5e6164be65e2
minhhung123 <prohungnguyenminh@gmail.com>
2023-01-29 18:24:54 +0700

    Add UI for wallet connect login screen

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '../src/components/layout';
+import { Login } from '../src/components/templates/login';
+
+const LoginPage = () => {
+    return (
+        <Layout>
+            <Login />
+        </Layout>
+    );
+};
+
+export default LoginPage;
